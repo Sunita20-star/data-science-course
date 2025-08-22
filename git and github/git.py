e0bfb771d3d4c376424f586564db9a1bb4a7722a
Sunita <sunitarai5870@gmail.com>
2025-08-22 20:40:48 +0545

    file modified

---
+++ git and github/git.py
@@ -0,0 +1,6 @@
+#flow of git
+#initialize git=git init
+#connect our local git to remote github=git remote add origin(key)
+#add files to git=git add file_name //git add .=to add all files in git
+#commit(message)=git commit -m "message"
+#push to the github=git push -u origin master/main
